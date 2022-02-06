ad4baf5a2375e6eba1262a0acdcc4a63228bf10a
hzms <h4zime@gmail.com>
2022-02-06 16:33:25 +0900

    loading svg helper

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -7,6 +7,17 @@ module IconHelper
     bars: '<path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>',
   }
 
+  LOADING_SVG_PATH = %q(
+    <svg class="animate-spin h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  )
+
+  def loading_svg(color: 'text-gray-500')
+    content_tag(:svg, class: "animate-spin h-5 w-5 #{color}", xmlsns: 'http://www.w3.org/2000/svg', fill: 'none', 'viewBox': '0 0 24 24') { LOADING_SVG_PATH.html_safe }
+  end
+
   def icon(name, opts = {})
     options = { size: 24, color: 'text-gray-500', class: nil }.merge(opts)
     css_class = options[:color]
