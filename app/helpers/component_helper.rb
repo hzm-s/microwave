e5c140396420ee6aeabbb734e91bd942fe9128b8
hzms <h4zime@gmail.com>
2022-02-10 19:03:19 +0900

    pend stimulus-dropdown

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,14 +1,69 @@
 module ComponentHelper
   include TagAttributeHelper
 
-  DROPDOWN_POSITION_CLASSES = {
-    left: 'origin-top-left left-0',
-    right: 'origin-top-right right-0',
+  COMPONENT_ATTRIBUTES = {
+    rv_container: {
+      data: { controller: 'reveal', reveal_hidden_class: 'hidden' },
+    },
+    rv_trigger: {
+      data: { action: 'click->reveal#toggle' },
+    },
+    rv_target: {
+      data: { reveal_target: 'item' },
+      class: 'hidden',
+    },
+    dd_container: {
+      data: { controller: 'reveal', reveal_hidden_class: 'hidden' },
+      class: 'relative inline-block',
+    },
+    dd_trigger: {
+      data: { action: 'click->reveal#toggle' },
+    },
+    dd_target: {
+      data: { reveal_target: 'item' },
+      class: 'transition hidden absolute z-50',
+    },
+    dd_container_pending: {
+      data: { controller: 'dropdown' },
+      class: 'relative inline-block',
+    },
+    dd_trigger_pending: {
+      data: { action: 'dropdown#toggle click@window->dropdown#hide' },
+    },
+    dd_target_pending: {
+      data: {
+        dropdown_target: 'menu',
+      },
+      class: 'transition hidden absolute z-50',
+    },
+    dd_target_left: {
+      data: {},
+      class: 'origin-top-left left-0',
+    },
+    dd_target_right: {
+      data: {},
+      class: 'origin-top-right right-0',
+    },
   }
 
+  def component_attrs(*args)
+    attr_sets = args.map do |arg|
+      if arg.is_a?(Symbol)
+        COMPONENT_ATTRIBUTES[arg]
+      else
+        arg
+      end
+    end
+
+    first, *rest = *attr_sets
+    return first if rest.empty?
+
+    rest.reduce(first) { |a, e| merge_tag_attrs(a, e) }
+  end
+
   def sort_list_options(extra = {})
-    url = extra.delete(:url)
-    group = extra.delete(:group)
+    url = extra.dup.delete(:url)
+    group = extra.dup.delete(:group)
 
     merge_tag_attrs(
       {
@@ -17,67 +72,6 @@ module ComponentHelper
           sort_list_url: url,
           sort_list_group: group,
         },
-      },
-      extra
-    )
-  end
-
-  def dd_container_options(extra = {})
-    merge_tag_attrs(
-      { data: { controller: 'dropdown' }, class: 'relative inline-block' },
-      extra
-    )
-  end
-
-  def dd_trigger_options(extra = {})
-    merge_tag_attrs(
-      { data: { action: 'dropdown#toggle click@window->dropdown#hide' } },
-      extra
-    )
-  end
-
-  def dd_target_options(extra = {})
-    position_css_class = DROPDOWN_POSITION_CLASSES[extra.delete(:position) || :left]
-    merge_tag_attrs(
-      {
-        data: { dropdown_target: 'menu' },
-        class: "hidden absolute z-50 #{position_css_class}",
-      },
-      extra
-    )
-  end
-
-  def rv_container_options(extra = {})
-    merge_tag_attrs(
-      { data: { controller: 'reveal', reveal_hidden_class: 'hidden' } },
-      extra
-    )
-  end
-
-  def rv_trigger_options(extra = {})
-    merge_tag_attrs(
-      { data: { action: 'click->reveal#toggle' } },
-      extra
-    )
-  end
-
-  def rv_target_options(extra = {})
-    merge_tag_attrs(
-      { data: { reveal_target: 'item' }, class: 'hidden' },
-      extra
-    )
-  end
-
-  def transition_options(extra = {})
-    merge_tag_attrs(
-      {
-        data: {
-          transition_enter_from: 'opacity-0 scale-95',
-          transition_enter_to: 'opacity-100 scale-100',
-          transition_leave_from: 'opacity-100 scale-100',
-          transition_leave_to: 'opacity-0 scale-95',
-        },
-        class: 'transition',
       },
       extra
     )
--- app/helpers/tag_attribute_helper.rb
+++ app/helpers/tag_attribute_helper.rb
@@ -1,7 +1,7 @@
 module TagAttributeHelper
   def merge_tag_attrs(default, extra)
-    css_class = default.delete(:class)
-    extra_css_class = extra.delete(:class)
+    css_class = default.dup.delete(:class)
+    extra_css_class = extra.dup.delete(:class)
     css_class = [css_class, "#{extra_css_class}"].compact.join(' ') if extra_css_class
 
     default.deep_merge(extra).merge(class: css_class)
