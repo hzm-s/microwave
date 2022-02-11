c9db0e703d9ce0dcafb9c14d97a6cf48c0d16d4d
hzms <h4zime@gmail.com>
2022-02-11 13:58:57 +0900

    register sentence types

--- config/initializers/types.rb
+++ config/initializers/types.rb
@@ -1,3 +1,5 @@
 require 'string_compatible_type'
 
-ActiveRecord::Type.register(:name, StringCompatibleType[:Name])
+ActiveRecord::Type.register(:name, StringCompatibleType['Name'])
+ActiveRecord::Type.register(:short_sentence, StringCompatibleType['Sentence::Short'])
+ActiveRecord::Type.register(:long_sentence, StringCompatibleType['Sentence::Long'])
