0e5d092d42d4c045f925437a2df2334ba186c810
hzms <h4zime@gmail.com>
2022-02-12 14:15:44 +0900

    create product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,5 +4,15 @@ describe Product do
   let(:name) { 'product name' }
   let(:vision) { 'product vision' }
 
-  let(:product) { described_class.create!(name: name, vision: vision) }
+  describe '.create' do
+    it do
+      product = described_class.create(name: name, vision: vision)
+
+      aggregate_failures do
+        expect(product.name.to_s).to eq name
+        expect(product.vision.to_s).to eq vision
+        expect(product.backlog).to_not be_nil
+      end
+    end
+  end
 end
