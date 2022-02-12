6ecaa827dfe23ea0c3dc7956d4066e4d421aab68
hzms <h4zime@gmail.com>
2022-02-12 14:14:54 +0900

    product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,24 +1,10 @@
 class Product < ApplicationRecord
-  class CurrentGoalNotAchieved < StandardError; end
-
   attribute :name, :name
   attribute :vision, :long_sentence
 
-  has_many :goals, class_name: 'ProductGoal', dependent: :destroy
-  has_one :current_product_goal, dependent: :destroy
+  has_one :backlog, class_name: 'ProductBacklog', dependent: :destroy
 
-  def current_goal
-    current_product_goal&.goal
-  end
-
-  def set_goal(content)
-    raise CurrentGoalNotAchieved if current_goal
-
-    self.goals.build(content: content)
-      .then { self.create_current_product_goal!(goal: _1) }
-  end
-
-  def achieve_current_goal
-    self.current_product_goal.destroy!
+  before_create do
+    self.build_backlog
   end
 end
