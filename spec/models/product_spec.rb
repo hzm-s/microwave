9be4023dbc464875c552270d267ee5de7a708b19
hzms <h4zime@gmail.com>
2022-02-13 13:35:37 +0900

    fix expectation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,21 +4,21 @@ describe Product do
   let(:name) { 'product name' }
   let(:vision) { 'product vision' }
 
-  describe '.create' do
+  describe 'initial' do
     it do
       product = described_class.create(name: name, vision: vision)
 
       aggregate_failures do
         expect(product.name.to_s).to eq name
         expect(product.vision.to_s).to eq vision
-        expect(product.backlog).to be_persisted
+        expect(product.reload.backlog).to_not be_nil
       end
     end
   end
 
-  let(:product) { described_class.create(name: name, vision: vision) }
+  let(:product) { described_class.create!(name: name, vision: vision) }
 
-  describe '#add_goal' do
+  describe 'add goal' do
     it do
       product.add_goal('1st goal')
       expect(product.reload.goals.map(&:content)).to eq ['1st goal']
