af7f670b2d90ff551b06d0eaaaf91f53346df713
hzms <h4zime@gmail.com>
2022-02-16 18:44:29 +0900

    add pbi UI

--- app/controllers/product_backlogs_controller.rb
+++ app/controllers/product_backlogs_controller.rb
@@ -1,11 +1,6 @@
 class ProductBacklogsController < ApplicationController
-  Requirement = Struct.new(:id, :description, :size, :criteria_count, keyword_init: true)
-
   def show
-    @requirements = [
-      Requirement.new(id: 100, description: 'POとしてプロダクトゴールを設定したい', size: 5, criteria_count: 6),
-      Requirement.new(id: 101, description: 'メンバーとしてプロダクトバックログアイテムを追加したい', size: 13, criteria_count: 0),
-      Requirement.new(id: 102, description: 'DEVとしてをプロダクトバックログアイテムの大きさを設定したい', size: 8, criteria_count: 5),
-    ]
+    product = Product.find(params[:product_id])
+    @items = product.backlog.items
   end
 end
