8fc80c1c401e40cbcc78d5bec1dbd55da6e046c1
hzms <h4zime@gmail.com>
2022-02-17 18:41:59 +0900

    clean up

--- app/models/product_backlog_item.rb
+++
@@ -1,5 +0,0 @@
-class ProductBacklogItem < ApplicationRecord
-  belongs_to :work
-
-  delegate :product_id, :number, :status, :description, :size, to: :work
-end
--- db/migrate/20220207101600_create_product_backlogs.rb
+++
@@ -1,9 +0,0 @@
-class CreateProductBacklogs < ActiveRecord::Migration[7.0]
-  def change
-    create_table :product_backlogs, id: :uuid do |t|
-      t.references :product, type: :uuid, foreign_key: true, index: { unique: true }
-
-      t.datetime :updated_at
-    end
-  end
-end
