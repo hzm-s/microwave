7b03ab10bd77ff0c7e166266b53902b6870f3b7b
hzms <h4zime@gmail.com>
2022-02-19 17:34:11 +0900

    init product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   attribute :name, :name
   attribute :vision, :long_sentence
+
+  has_one :backlog, class_name: 'ProductBacklog'
+
+  before_create do
+    self.build_backlog
+  end
 end
