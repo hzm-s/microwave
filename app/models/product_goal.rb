853a82fa2a0431800ea83f66351d3674d43216b2
hzms <h4zime@gmail.com>
2022-02-20 10:48:59 +0900

    achieve product goal

--- app/models/product_goal.rb
+++ app/models/product_goal.rb
@@ -1,15 +1,41 @@
 class ProductGoal < ApplicationRecord
+  class AlreadyAchieved < StandardError; end
+
   attribute :content, :long_sentence
 
   enum :status, unachieved: 0, achieved: 1, default: :unachieved
 
   validates :content, presence: true, domain_object: true
 
-  has_one :unachieved, class_name: 'UnachievedProductGoal'
-  has_one :achieved, class_name: 'AchievedProductGoal'
+  has_one :unachieved, class_name: 'UnachievedProductGoal', autosave: true
+  has_one :achieved, class_name: 'AchievedProductGoal', autosave: true
+
+  scope :unachieved, -> { joins(:unachieved) }
 
   before_create do
-    self.status = 'unachieved'
-    self.build_unachieved
+    self.status = :unachieved
+    update_status_detail
+  end
+
+  before_update :update_status_detail
+
+  def achieve!
+    raise AlreadyAchieved if achieved?
+
+    self.tap { achieved! }
+  end
+
+  private
+
+  def update_status_detail
+    return if persisted? && !status_changed?
+
+    if unachieved?
+      self.achieved&.mark_for_destruction
+      self.unachieved || self.build_unachieved
+    else
+      self.unachieved&.mark_for_destruction
+      self.achieved || self.build_achieved(achieved_at: Time.current)
+    end
   end
 end
--- spec/models/product_goal_spec.rb
+++ spec/models/product_goal_spec.rb
@@ -10,7 +10,7 @@ describe ProductGoal do
       aggregate_failures do
         expect(goal).to be_persisted
         expect(goal.content.to_s).to eq '1st goal'
-        expect(goal.status).to eq 'unachieved'
+        expect(goal).to be_unachieved
         expect(goal.unachieved).to_not be_nil
         expect(goal.achieved).to be_nil
       end
@@ -30,6 +30,29 @@ describe ProductGoal do
     it do
       model = described_class.new(valid.merge(content: 'a' * 1001))
       expect(model).to_not be_valid
+    end
+  end
+
+  describe 'achieve' do
+    let(:goal) { described_class.create!(content: 'the goal') }
+
+    context 'when unachieved' do
+      it do
+        goal.achieve!.tap { _1.reload }
+
+        aggregate_failures do
+          expect(goal).to be_achieved
+          expect(goal.unachieved).to be_nil
+          expect(goal.achieved).to_not be_nil
+        end
+      end
+    end
+
+    context 'when achieved' do
+      it do
+        goal.achieve!
+        expect { goal.achieve! }.to raise_error described_class::AlreadyAchieved
+      end
     end
   end
 end
