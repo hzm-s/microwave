83b5f530fd7a125aaa298ec2d9ebefe94a11bdc7
hzms <h4zime@gmail.com>
2022-03-19 19:54:06 +0900

    fix

--- app/models/active_user.rb
+++ app/models/active_user.rb
@@ -9,12 +9,10 @@ class ActiveUser < ApplicationRecord
 
   class << self
     def new_with_account(name:, email:, account:)
-      candidate = new(name: name, email: email) { _1.accounts << account }
-      candidate.validate
-      candidate.errors.delete(:user)
-      return candidate if candidate.errors.any?
-
-      candidate.tap { _1.initials = _1.email.initials }
+      new(name: name, email: email) do |au|
+        au.accounts << account
+        au.initials = au.email&.initials
+      end
     end
   end
 end
