108c07e850afc3050ceb8a7996a55be7d6454bd6
hzms <h4zime@gmail.com>
2022-03-31 18:33:12 +0900

    follow new I/F to add team member

--- app/usecases/register_team_with_member_usecase.rb
+++ app/usecases/register_team_with_member_usecase.rb
@@ -1,7 +1,7 @@
 class RegisterTeamWithMemberUsecase < BaseUsecase
   def perform(name:, user:, roles:)
     team = Team.new(name: name)
-    team.add_member(TeamMember.new(user: user, roles: roles))
+    team.add_member(user: user, roles: roles)
 
     if team.save
       succeeded(team: team)
