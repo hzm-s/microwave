24d26b2d554e14eeb07e1fd6cc988876f5fad9a4
hzms <h4zime@gmail.com>
2022-04-12 19:15:21 +0900

    product belongs to owner user

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe Product do
   let(:name) { 'product name' }
   let(:vision) { 'product vision' }
+  let(:user) { sign_up }
 
   describe 'initial' do
     it do
-      product = described_class.create(name: name, vision: vision)
+      product = described_class.create(owner: user, name: name, vision: vision)
 
       aggregate_failures do
+        expect(product.owner).to eq user
         expect(product.name.to_s).to eq name
         expect(product.vision.to_s).to eq vision
         expect(product.goals).to be_empty
@@ -19,6 +21,7 @@ describe Product do
   describe 'validation' do
     let(:valid) do
       {
+        owner: user,
         name: 'product name',
         vision: 'product vision',
       }
@@ -26,6 +29,10 @@ describe Product do
 
     it do
       expect(described_class.new(valid)).to be_valid
+    end
+
+    it do
+      expect(described_class.new(valid.merge(owner: nil))).to_not be_valid
     end
 
     it do
@@ -47,7 +54,7 @@ describe Product do
 
   describe 'add development team' do
     it do
-      product = described_class.create!(name: 'the product', vision: 'vision')
+      product = described_class.create!(owner: user, name: 'the product', vision: 'vision')
       team = create_team
       product.teams << team
       expect { product.teams << team }.to raise_error ActiveRecord::RecordNotUnique
