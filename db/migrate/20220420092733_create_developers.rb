ab04404989172d86e1bfa1a1dc1cb431daf943ef
hzms <h4zime@gmail.com>
2022-04-22 18:18:11 +0900

    persist developer

---
+++ db/migrate/20220420092733_create_developers.rb
@@ -0,0 +1,10 @@
+class CreateDevelopers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :developers do |t|
+      t.references :user, type: :uuid, null: false, foreign_key: true
+      t.references :team, type: :uuid, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
