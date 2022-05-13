fb20c492ace95bc19b7adae335436b67c5777516
hzms <h4zime@gmail.com>
2022-05-14 08:39:48 +0900

    add team to scrum master action

--- app/controllers/team/scrum_masters_controller.rb
+++ app/controllers/team/scrum_masters_controller.rb
@@ -1,5 +1,19 @@
 class Team::ScrumMastersController < ApplicationController
+  helper_method :current_team
+
   def show
     @scrum_master = ScrumMaster.joins(:leadings).find_by(team_leadings: { team_id: params[:team_id] })
+  end
+
+  def create
+    @scrum_master = ScrumMaster.find_or_initialize_by(user_id: current_user.id)
+    @scrum_master.add_team(current_team)
+    @scrum_master.save
+  end
+
+  private
+
+  def current_team
+    @current_team ||= Team.find_by(id: params[:team_id])
   end
 end
--- spec/requests/team/scrum_masters_spec.rb
+++ spec/requests/team/scrum_masters_spec.rb
@@ -4,6 +4,10 @@ describe '/team/:team_id/scrum_master' do
   let(:user) { sign_up }
   let(:team) { create_team }
 
+  before do
+    sign_in(user)
+  end
+
   describe 'show' do
     it do
       add_scrum_master(team, user)
@@ -11,6 +15,17 @@ describe '/team/:team_id/scrum_master' do
       get team_scrum_master_path(team_id: team.id)
 
       expect(response.body).to include user.name
+    end
+  end
+
+  describe 'create' do
+    context 'precondition is satisfied' do
+      it do
+        post team_scrum_master_path(team_id: team.id, format: :turbo_stream)
+
+        get team_scrum_master_path(team_id: team.id)
+        expect(response.body).to include user.name
+      end
     end
   end
 end
